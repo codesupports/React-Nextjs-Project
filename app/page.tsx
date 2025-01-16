eff2a74b1f12ff33c65ef85d2409e16ecd9fd0f2
codesupports <rajkumar.mah55@gmail.com>
2025-01-16 22:13:51 +0530

    es lint deployment sone rule disable

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,14 @@ import Header from "./src/components/header";
 import Product from "./src/components/product";
 import Data from "./src/data/data.json";
 
-interface MyComponentProps {
-  data: any;
-}
 
-const Home: React.FC<MyComponentProps> = () => {
 
+const Home = () => {
   return (
     <div >
       <div className="container mx-auto px-4">
       {/* <About /> */}
       <Header />
-      
       <Product data={Data} />
       </div>
     </div>
