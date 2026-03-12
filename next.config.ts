213497d3e6033703c7b779f4f461d9684ac73239
codesupports <rajkumar.mah55@gmail.com>
2026-03-12 21:27:24 +0530

    fix image issue

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+const repo = "React-Nextjs-Project";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export"
+  output: "export",
 };
 
 export default nextConfig;
 module.exports = {
-//   basePath: '/React-Nextjs-Project', // Replace 'your-repo-name' with your actual repo name
+basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true,         // Disable image optimization (optional)
   },
